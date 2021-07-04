fd32f6c405eb6b15ee37c396fe5a1ea26c4d9e0f
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-04 23:01:12 +0530

    Update maaChodDiVro.py

--- TelethonBot/plugins/maaChodDiVro.py
+++ TelethonBot/plugins/maaChodDiVro.py
@@ -5,14 +5,6 @@ from telethon import events, Button
 
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/smex"))
 async def _(event):
- k = [[Button.text("/start"), Button.text("/start")]]
- k += [[Button.text("/start"), Button.text("/start")]]
- k += [[Button.text("/start"), Button.text("/start")]]
- k += [[Button.text("/start"), Button.text("/start")]]
- k += [[Button.text("/start"), Button.text("/start")]]
- k += [[Button.text("/start"), Button.text("/start")]]
- k += [[Button.text("/start"), Button.text("/start")]]
- k += [[Button.text("/start"), Button.text("/start")]]
- k += [[Button.text("/start"), Button.text("/start")]]
+ k = [[Button.text("comrade rabdi vai"), Button.text("comrade randi vai")]]
  for x in range(30):
   await bot.send_message(event.chat.id, "Hm", buttons=k)
