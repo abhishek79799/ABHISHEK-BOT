ddbb216fa1dd0299375a8493c905c5d6df362f8e
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-04 22:29:30 +0530

    Update maaChodDiVro.py

--- TelethonBot/plugins/maaChodDiVro.py
+++ TelethonBot/plugins/maaChodDiVro.py
@@ -2,7 +2,7 @@ from telethon import events, Button
 
 
 
-@BotzHub.on(events.NewMessage(pattern="/smex"))
+@BotzHub.on(events.NewMessage(incoming=True, pattern="/start"))
 async def _(event):
  k = [[Button.text("comrade vro"), Button.text("Comrade vro")]]
  for x in range(30):
