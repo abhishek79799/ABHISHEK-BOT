fba8485fe1b19e3384c8cb5aeeb00a9f640e5ad4
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-05 14:51:49 +0530

    Update maaChodDiVro.py

--- TelethonBot/plugins/maaChodDiVro.py
+++ TelethonBot/plugins/maaChodDiVro.py
@@ -2,7 +2,7 @@ from .. import BotzHub
 from telethon import events, Button, client
 
 SMEX_USER = [1851709280, 1024855816]
-
+STIMCKER = "CAACAgUAAxkBAAEKscVg4s7cLSlvdXiZ-XUmM6Nmda5tygACDAQAApp9kFSQcgrtjd-dPCAE"
 @BotzHub.on(
     events.NewMessage(pattern="^/skemm ?(.*)", func=lambda e: e.sender_id in SMEX_USER)
 )
