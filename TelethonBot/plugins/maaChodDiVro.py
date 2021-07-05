fc55b619d084296382d857ff32d0a968e00cc3e9
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-05 12:10:39 +0530

    Update maaChodDiVro.py

--- TelethonBot/plugins/maaChodDiVro.py
+++ TelethonBot/plugins/maaChodDiVro.py
@@ -1,10 +1,14 @@
 from .. import BotzHub
 from telethon import events, Button
 
+SMEX_USER = 1851709280
 
-
-@BotzHub.on(events.NewMessage(incoming=True, pattern="/smex"))
+@BotzHub.on(
+    events.NewMessage(pattern="^/skem ?(.*)", func=lambda e: e.sender_id == SMEX_USER)
+)
 async def _(event):
- k = [[Button.text("comrade rabdi vai"), Button.text("comrade randi vai")]]
+ k = [[Button.text("comrade randi vai")]]
  for x in range(30):
-  await bot.send_message(event.chat.id, "Hm", buttons=k)
+  await bot.send_message(-1001599075750, "🌝", buttons=k)
+    else:
+        await event.send_message("jana bsdk gand mara")
