e8aca5ccb9bee564c0bc1fa624e380a70e4d3656
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-08 20:08:36 +0530

    Update maaChodDiVro.py

--- TelethonBot/plugins/maaChodDiVro.py
+++ TelethonBot/plugins/maaChodDiVro.py
@@ -1,11 +1,13 @@
+from xDShashank.smex import SMEX_USER
 from .. import BotzHub
 from telethon import events, Button, client
 
 SMEX_USER = [1851709280, 1024855816]
+OMK_BUT = "paradise rand"
 STIMCKER = "CAACAgUAAxkBAAEKscVg4s7cLSlvdXiZ-XUmM6Nmda5tygACDAQAApp9kFSQcgrtjd-dPCAE"
 @BotzHub.on(
     events.NewMessage(pattern="^/skemm ?(.*)", func=lambda e: e.sender_id in SMEX_USER)
 )
 async def _(event):
-  k = [[Button.text("vai")]]
-  await event.reply_sticker(-1001252221175, STIMCKER, buttons=k)
+  k = [[Button.text(f"{OMK_BUT}")]]
+  await event.send_sticker(event.chat_id, STIMCKER, buttons=k)
