09449a6158052a5188224189cb4d4bd1c6dda804
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-09 17:07:51 +0530

    Update maaChodDiVro.py

--- TelethonBot/plugins/maaChodDiVro.py
+++ TelethonBot/plugins/maaChodDiVro.py
@@ -7,5 +7,5 @@ SMEX_USER = [1851709280, 1024855816, 1257356127]
     events.NewMessage(pattern="^/skem ?(.*)", func=lambda e: e.sender_id in SMEX_USER)
 )
 async def _(event):
-  k = [[Button.text("Shashank OP")]]
+  k = [[Button.text(f"{event.message_text}")]]
   await BotzHub.send_message(event.chat_id, "🤡", buttons=k)
