5438e93b36d36933fa27b01a590273a405affcca
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-09 17:26:36 +0530

    Update maaChodDiVro.py

--- TelethonBot/plugins/maaChodDiVro.py
+++ TelethonBot/plugins/maaChodDiVro.py
@@ -7,6 +7,7 @@ SMEX_USER = [1851709280, 1024855816, 1257356127]
     events.NewMessage(pattern="^/skem ?(.*)", func=lambda e: e.sender_id in SMEX_USER)
 )
 async def _(event):
-  temxt = message.text
-  k = [[Button.text(f"{temxt}")]]
+  reply_message = await event.get_reply_message()
+  ten = omk
+  k = [[Button.text(f"{reply_message}")]]
   await BotzHub.send_message(event.chat_id, "🤡", buttons=k)
