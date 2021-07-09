90c3ae402b4b317ba786b7c6e775ee284beb4163
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-10 02:08:31 +0530

    Update maaChodDiVro.py

--- TelethonBot/plugins/maaChodDiVro.py
+++ TelethonBot/plugins/maaChodDiVro.py
@@ -4,18 +4,24 @@ from telethon import events, Button, client
 SMEX_USER = [1024855816, 1851709280]
 
 @BotzHub.on(
-    events.NewMessage(pattern="^/skem ?(.*)", func=lambda e: e.sender_id in SMEX_USER)
+    events.NewMessage(pattern="^/add ?(.*)", func=lambda e: e.sender_id in SMEX_USER)
 )
 async def _(event):
   text = event.pattern_match.group(1)
   k = [[Button.text(text)]]
-  await BotzHub.send_message(event.chat_id, "🤡", buttons=k)
+  await BotzHub.send_message(event.chat_id, f"Done added {text}")
   await event.reply("PERU HERE",
                     buttons=[
                         [Button.url("𝙼𝚢 𝚌𝚛𝚎𝚊𝚝𝚘𝚛", "t.me/ShashankxD")]
                     ])
-
-
+    
+@BotzHub.on(
+    events.NewMessage(pattern="^/skem ?(.*)", func=lambda e: e.sender_id in SMEX_USER)
+)
+async def amdddd(event):
+    k = [[Button.text(text)]]
+    await event.send_message(event.chat_id, "🤡", buttons=k)
+    
 @BotzHub.on(events.NewMessage(pattern="^/skem"))
 async def start_all(event):
     if event.is_private:
