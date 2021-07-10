8875b2a05c13aa4d7c6bdf2d83ade0908f932343
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-10 18:32:15 +0530

    Update omk.py

--- TelethonBot/plugins/omk.py
+++ TelethonBot/plugins/omk.py
@@ -1,6 +1,6 @@
 from .. import BotzHub
 from telethon import event
 
-@BotzHub.on(pattern="/skip")
+@BotzHub.on(events.NewMessage(pattern="/skip"))
 async def oommkkj(event):
   await event.send(event.chat_id, "**jana lwde**")
