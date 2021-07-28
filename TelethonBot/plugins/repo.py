99bc56ef470456cff96558c26f61b620f720a2b3
abhishek79799 <79202215+abhishek79799@users.noreply.github.com>
2021-07-28 13:02:53 +0530

    Update repo.py

--- TelethonBot/plugins/repo.py
+++ TelethonBot/plugins/repo.py
@@ -6,6 +6,6 @@ SMEX_USER = [1528243010]
 @BotzHub.on(events.NewMessage(pattern="^/repo ?(.*)"))
 async def _(event):
   if event.sender_id in SMEX_USER:
-    await BotzHub.send_message("**HEY MASTER HERE IS YOUR REPO**\n\nhttps://github.com/PANDITOP/PANDITBOT")
+    await BotzHub.send_message("**HEY MASTER HERE IS YOUR REPO**\n\nhttps://github.com/abhishek79799/abhishek79799")
   else:
     await event.reply("**BHAI YAAR THUM GAAND MARAO**")
